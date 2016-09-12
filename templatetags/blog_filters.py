2eb9f78b56b97624b970830d7b5c70e910442c9a
Pedro Alves <pta2002@pta2002.com>
2016-09-12 13:09:09 +0100

    Added linkify filters

--- templatetags/blog_filters.py
+++ templatetags/blog_filters.py
@@ -1,7 +1,29 @@
 from django import template
+from urllib.parse import urlparse
 import markdown2, re, bleach
 
 register = template.Library()
+
+def external_links(attrs, new=False):
+    o = urlparse(attrs['href'])
+    if o.netloc != 'pta2002.com':
+        attrs['target'] = '_blank'
+    return attrs
+
+
+def handle_pytld(attrs, new=False):
+    if not new:  # This is an existing <a> tag, leave it be.
+        return attrs
+
+    # If the TLD is '.py', make sure it starts with http: or https:
+    href = attrs['href']
+    if href.endswith('.py') and not href.startswith(('http:', 'https:')):
+        # This looks like a Python file, not a URL. Don't make a link.
+        return None
+
+    # Everything checks out, keep going to the next callback.
+    return attrs
+
 
 @register.filter(name='markdown')
 def domarkdown(value, safe="escape"):
@@ -22,4 +44,4 @@ def tobootstrap(value):
 
 @register.filter(name='linkify')
 def linkify(value):
-    return bleach.linkify(value)
+    return bleach.linkify(value, callbacks=[external_links, handle_pytld], parse_email=True)
